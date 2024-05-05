6d15b19b9e3be881f26f84ea41383560af5619b6
pbiswas1208 <124027457+pbiswas1208@users.noreply.github.com>
2024-05-05 20:55:08 +0530

    AddingLogo

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,37 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import UserService from '../service/UserService';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import UserService from "../service/UserService";
+import { useNavigate } from "react-router-dom";
+import { Image } from "react-bootstrap";
+
+import logo from "../images/logo.png";
 
 function Navbar() {
-    const isAuthenticated = UserService.isAuthenticated();
-    const isAdmin = UserService.isAdmin();
-    const navigate = useNavigate();
+  const isAuthenticated = UserService.isAuthenticated();
+  const isAdmin = UserService.isAdmin();
+  const navigate = useNavigate();
 
-
-
-    const handleLogout = () => {
-        const confirmDelete = window.confirm('Are you sure you want to logout this user?');
-        if (confirmDelete) {
-            UserService.logout();
-        }
-        
-        navigate("/login")
-    };
-
-
-    return (
-        <nav styles='margin-bottom: 10px'>
-            <ul>
-                {!isAuthenticated && <li><Link to="/">PolyGlot - Language Learning Application</Link></li>}
-                {isAuthenticated && <li><Link to="/profile">Profile</Link></li>}
-                {isAuthenticated && <li><Link to="/masterCourses">Explore Courses</Link></li>}
-                {isAuthenticated && <li><Link to="/myCourses">My Courses</Link></li>}
-                {isAuthenticated && <li><Link to="/progress">Progress Tracking</Link></li>}
-                {isAuthenticated && <li><Link to="/" onClick={handleLogout}>Logout</Link></li>}
-            </ul>
-        </nav>
+  const handleLogout = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to logout this user?"
     );
+    if (confirmDelete) {
+      UserService.logout();
+    }
+
+    navigate("/login");
+  };
+
+  return (
+    <nav
+      style={{
+        marginBottom: 10,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image src={logo} rounded style={{ width: 48 }} />
+      <ul style={{ marginBottom: 0 }}>
+        {!isAuthenticated && (
+          <li>
+            <Link to="/">PolyGlot - Language Learning Application</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <Link to="/masterCourses">Explore Courses</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <Link to="/myCourses">My Courses</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <Link to="/progress">Progress Tracking</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
